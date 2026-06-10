6447c015bac1a10c098a4dc838a6422b4acd0127
Gustavo Adolfo Bolaño Ramos <gustavoadolfobolano@gmail.com>
2026-06-10 15:37:08 -0500

    feat(fix):Correcciones menores en application.yml

--- back/src/main/resources/data.sql
+++ back/src/main/resources/data.sql
@@ -3,10 +3,10 @@ VALUES('Maribel Ramirez', '0002', 178500.00, 'activa', '2026-06-07 00:00:00', 28
 ('Jorge Segura', '0001', 95200.00, 'activa', '2026-06-06 00:00:00',15200.00,80000.00,'supervisor');
 
 INSERT INTO DETALLE_FACTURA(cantidad,precio_unitario, producto,subtotal,factura_id)
-VALUES(2 ,30000.00,Blusa manga corta, 60000.00, 2)
-  (2 ,45000.00,Falda de jean larga, 90000.00, 2)
-  (1 ,50000.00,camisa manga larga, 50000.00, 1)
-  (1 ,30000.00,pantalon clasico, 30000.00, 1);
+VALUES(2 ,30000.00,'Blusa manga corta', 60000.00, 2),
+  (2 ,45000.00,'Falda de jean larga', 90000.00, 2),
+  (1 ,50000.00,'camisa manga larga', 50000.00, 1),
+  (1 ,30000.00,'pantalon clasico', 30000.00, 1);
 
 
 
